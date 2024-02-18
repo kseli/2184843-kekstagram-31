d199ba2f465750ad34e4095075eb3d458b87f4d3
Екатерина <kse.lif@list.ru>
2024-02-18 16:37:34 +0300

    Удаляет ошибки eslint

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,7 @@
 
 const lengthOfString = function (string, maxLength) {
   return string.length <= maxLength;
-}
+};
 
 lengthOfString('Тестовая строка', 14);
 
@@ -13,14 +13,14 @@ lengthOfString('Тестовая строка', 14);
 //Функция для проверки, является ли строка палиндромом.
 
 const isPalindrom = function (string) {
-  let normalString = (string.replaceAll(' ', '')).toLowerCase();
+  const normalString = (string.replaceAll(' ', '')).toLowerCase();
   let testString = '';
 
-  for (i = normalString.length - 1; i >= 0; i--) {
+  for (let i = normalString.length - 1; i >= 0; i--) {
     testString += normalString[i];
   }
 
   return testString === normalString;
-}
+};
 
 isPalindrom('доВод');
